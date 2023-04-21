bc2473fe1e8b525a63a963345518717327e2743b
meti98 <m.schaider98@gmail.com>
2023-04-21 11:41:25 +0200

    updagte

--- api/index.py
+++ api/index.py
@@ -1,19 +1,17 @@
 """Main entrypoint for the app."""
 from langchain.embeddings.openai import OpenAIEmbeddings
 from .qa_chain import load_qa_chain
-from pydantic import BaseModel
 from qdrant_client import QdrantClient
 from langchain.vectorstores import Qdrant
 import os
 import logging
-from fastapi import FastAPI
 
 """ openai.log = "debug" """
 
 app = FastAPI()
 
 
-async def init():
+def init():
     if "docsearch" in globals():
         return
     logging.info("loading vectorstore")
@@ -26,11 +24,24 @@ async def init():
     chain = load_qa_chain(docsearch)
 
 
-class Question(BaseModel):
-    question: str
+from http.server import BaseHTTPRequestHandler
+import json
+ 
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        try:
+            data = json.loads(post_data)
+            init()
+            response = chain({"question": data["question"]})
+            self.send_response(200)
+        except json.JSONDecodeError:
+            response = {
+                'error': 'Invalid JSON data'
+            }
+            self.send_response(400)
 
-
-@app.post("/api")
-async def post(question: Question):
-    await init()
-    return chain({"question": question.question})
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(response).encode())
